870e7fedbe97f9cc1fb9ed24ee50ddee29a61554
edmharalcones <134608637+edmharalcones@users.noreply.github.com>
2023-10-08 19:59:48 +0800

    modified:   resources/views/welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -222,11 +222,17 @@
                             if (!$result) {
                                 die("Invalid query: " . $connection->error);
                             }
-                            echo '<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">';
+                            echo '<div id="slider" class="carousel slide carousel-dark text-center" data-interval="false">';
+                            echo '<button class="carousel-control-prev" type="button" data-bs-target="#slider" data-bs-slide="prev">';
+                            echo '<span class="carousel-control-prev-icon"></span>';
+                            echo '</button>';
+                            echo '<button class="carousel-control-next" type="button" data-bs-target="#slider" data-bs-slide="next">';
+                            echo '<span class="carousel-control-next-icon"></span>';
+                            echo '</button>';
                             echo '<div class="carousel-inner">';
 
                             while ($row = $result->fetch_assoc()) {
-                              echo '<div class="carousel-item ">';
+                              echo '<div class="carousel-item active ">';
                               echo '<img src="' . $row['photo'] . '" class="d-block w-100" alt="' . $row['name'] . '">';
                               echo '<div class="carousel-caption d-none d-md-block">';
                               echo '<h5>' . $row['name'] . '</h5>';
@@ -235,16 +241,7 @@
                               echo '</div>';
                             }
                               echo '</div>';
-                              echo '<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">';
-                              echo '<span class="carousel-control-prev-icon" aria-hidden="true"></span>';
-                              echo '<span class="sr-only">Previous</span>';
-                              echo '</a>';
-                              echo '<a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">';
-                              echo '<span class="carousel-control-next-icon" aria-hidden="true"></span>';
-                              echo '<span class="sr-only">Next</span>';
-                              echo '</a>';
-                              echo '</div>';
-                          $db = null;
+                         $db = null;
 
     ?>
     
