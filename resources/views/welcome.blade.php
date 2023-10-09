0d0e7909d83bc018a1ebc3b9cb2a9cdff1aa45be
edmharalcones <134608637+edmharalcones@users.noreply.github.com>
2023-10-09 12:24:02 +0800

    modified:   resources/views/welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -234,10 +234,8 @@
                             while ($row = $result->fetch_assoc()) {
                               echo '<div class="carousel-item active ">';
                               echo '<img src="' . $row['photo'] . '" class="d-block w-100" alt="' . $row['name'] . '">';
-                              echo '<div class="carousel-caption d-none d-md-block">';
                               echo '<h5>' . $row['name'] . '</h5>';
                               echo '<p>' . $row['review'] . '</p>';
-                              echo '</div>';
                               echo '</div>';
                             }
                               echo '</div>';
