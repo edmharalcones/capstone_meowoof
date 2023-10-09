dec2a1e19c45e63ccd4882ffc4addbde620f1897
edmharalcones <134608637+edmharalcones@users.noreply.github.com>
2023-10-09 13:45:24 +0800

    modified:   resources/views/about.blade.php 	modified:   resources/views/contact.blade.php 	modified:   resources/views/donate.blade.php 	modified:   resources/views/faq.blade.php 	modified:   resources/views/petcare.blade.php 	modified:   resources/views/pets.blade.php 	modified:   resources/views/review.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,7 +7,7 @@
         <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="{{ url('/css/style.css') }}" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script> 
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -133,7 +133,7 @@
 
       <div class="col" style="margin-top: 5%;">
         <button class="btn-petcare shadow-none border-0" type="submit">Send</button>
-        <input type="hidden" name="_next" value="https://edmharalcones.github.io/MEOWOOF/contact">
+        <input type="hidden" name="_next" value="https://concertspot.online/public/contact">
     
       </div>
     </form>  
