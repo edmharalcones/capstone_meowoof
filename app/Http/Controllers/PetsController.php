ad58e5191a24762220aa52c18b44a17d1679b2c9
edmharalcones <134608637+edmharalcones@users.noreply.github.com>
2023-10-15 12:29:15 +0800

    new file:   app/Http/Controllers/PetsController.php 	new file:   app/Models/Pets.php 	modified:   resources/views/dashboard.blade.php

---
+++ app/Http/Controllers/PetsController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\DB;
+use App\Models\Pets;
+
+class PetsController extends Controller
+{
+    
+    public function delete($id)
+    {
+    DB::table('reviews')->where('id', $id)->delete();
+    return redirect()->back();
+    }
+    public function store(Request $request)
+    {
+        try {
+    
+            $event = Pets::create([
+                'link' => $request->link,
+            ]);
+    
+            if ($request->hasFile('pet') && $request->hasFile('cover')) {
+                $petFile = $request->file('pet');
+                $coverFile = $request->file('cover');
+    
+                $petFilename = time() . $petFile->getClientOriginalName();
+                $coverFilename = time() . $coverFile->getClientOriginalName();
+    
+                $petFilePath = $petFile->storeAs('images', $petFilename, 'public');
+                $coverFilePath = $coverFile->storeAs('images', $coverFilename, 'public');
+    
+                $petStoragePath = 'storage/' . $petFilePath;
+                $coverStoragePath = 'storage/' . $coverFilePath;
+    
+                $event->pet = $petStoragePath;
+                $event->cover = $coverStoragePath;
+                $event->save();
+            }
+    
+            return back();
+        } catch (\Exception $e) {
+            \Log::error('Database error: ' . $e->getMessage());
+            return back()->withInput()->withErrors(['error' => 'Duplicate.']);
+        }
+    }
+    
+
+
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,10 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Reviews') }}
+        </h2>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Pets') }}
         </h2>
     </x-slot>
 
