55f7c4eb28c8a8f273a16289d612f47e3b353315
edmharalcones <134608637+edmharalcones@users.noreply.github.com>
2023-10-15 13:46:01 +0800

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -22,31 +22,31 @@ Route::get('/', function () {
 
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
 
 Route::get('/contact', function () {
     return view('contact');
-});
+})->name('contact');
 
 Route::get('/review', function () {
     return view('review');
-});
+})->name('review');
 
 Route::get('/donate', function () {
     return view('donate');
-});
+})->name('donate');
 
 Route::get('/faq', function () {
     return view('faq');
-});
+})->name('faq');
 
 Route::get('/petcare', function () {
     return view('petcare');
-});
+})->name('petcare');
 
 Route::get('/pets', function () {
     return view('pets');
-});
+})->name('pets');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
